e5217ed5f4d24c4f6a4ccf7faebc2dfb801cdace
danishshirsangi <danishshirsangi@gmail.com>
2023-04-28 00:27:49 +0530

    [Recording Classes Count]

--- student/views.py
+++ student/views.py
@@ -11,7 +11,7 @@ from .models import Student, Department, Division, TempFileTest, Classrooms
 from .predict import predict
 from .threading import CameraWidget
 import datetime
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 
 s = {}
 
@@ -21,10 +21,11 @@ def home_redirect(request):
 def gen_frames(model, dept ,div):
     camera = cv2.VideoCapture(0)
     global s
-    s[dept+div] = set()
+    s[dept+div] = []
     while True:
         _, frame = camera.read()
-        s[dept+div].add(tuple(predict(frame, model)))
+        s[dept+div].append([predict(frame, model), str(datetime.datetime.now().strftime("%H:%M:%S")), str(datetime.datetime.now().strftime("%m/%d/%Y"))])
+        #s[dept+div].add(tuple(predict(frame, model)))
         #print(frame.shape)
         frame = cv2.imencode('.jpeg', frame)[1].tobytes()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -51,17 +52,88 @@ def vidfeed(request, dept, div):
     #return StreamingHttpResponse(t1.gen_frames(),content_type='multipart/x-mixed-replace; boundary=frame')
 
 def test_page(request, dept, div):
+    workbook_name = 'attendance_xl.xlsx'
+
     if request.method == "POST":
+
+        if os.path.isfile(workbook_name):
+            workbook = load_workbook(workbook_name)
+        else:
+            workbook = Workbook()
+        
+        if dept+div in workbook.sheetnames:
+            worksheet = workbook[dept+div]
+        else:
+            worksheet = workbook.create_sheet(dept+div)
+
+        attendances = s.get(dept+div, None)
+        #print(attendances)
+
+        dics = {}
+
+        for each_frame in attendances:
+            for usn in each_frame[0]:
+                dics[usn] = [each_frame[1]]
+
+
+        for dic_usn in dics.keys():
+            for cells in worksheet.iter_cols(min_col=1, max_col=1, values_only=True):
+                for xl_usn in range(len(cells)):
+                    if dic_usn == cells[xl_usn]:
+                        worksheet['B'+str(xl_usn+1)].value = dics[dic_usn][0]
+                        worksheet['C'+str(xl_usn+1)].value = worksheet['C'+str(xl_usn+1)].value + 1
+                        break
+                else:
+                    endcell_index = worksheet.max_row + 1
+                    end_cell = worksheet.cell(row=worksheet.max_row+1,column=1)
+                    end_cell.value = dic_usn
+                    worksheet['B'+str(endcell_index)].value = dics[dic_usn][0]
+                    worksheet['C'+str(endcell_index)].value = 1
+
+        for i in range(worksheet.max_row):
+            if worksheet['D'+str(i+1)].value is not None:
+                worksheet['D'+str(i+1)].value = worksheet['D'+str(i+1)].value + 1
+            else:
+                worksheet['D'+str(i+1)].value = 1
+                    
+        workbook.save(workbook_name)
+        '''for each_frame in attendances:
+            for usn in each_frame[0]:
+                for cells in worksheet.iter_cols(min_col=1, max_col=1, values_only=True):
+                    for xl_usn in range(len(cells)):
+
+                        if worksheet['D'+str(xl_usn+1)] is not None:
+                            worksheet['D'+str(xl_usn+1)].value = worksheet['D'+str(xl_usn+1)].value + 1
+                        else:
+                            worksheet['D'+str(xl_usn+1)].value = 1
+                        
+                        if cells[xl_usn] == usn:
+                            if worksheet['C'+str(xl_usn+1)] is not None:
+                                worksheet['C'+str(xl_usn+1)].value = worksheet['C'+str(xl_usn+1)].value + 1
+                            else:
+                                worksheet['C'+str(xl_usn+1)].value = 1
+
+                    #print(usn, each_frame[1], each_frame[2])'''
+
+        #workbook.save('attendance_xl.xlsx')
+        
+        '''
         workbook = Workbook()
         sheet = workbook.active
         col = 1
         global s
         attendances = s.get(dept+div)
         for attende in attendances:
-            sheet['A'+str(col)] = attende[0]
-            sheet['B'+str(col)] = datetime.datetime.now().strftime('%H:%M:%S')
-            col += 1
+            print(attende)
+            if len(attende) > 0:
+                sheet['A'+str(col)] = attende[0]
+                sheet['B'+str(col)] = datetime.datetime.now().strftime('%H:%M:%S')
+                col += 1
+            else:
+                pass
         workbook.save(filename="hello_world.xlsx")
+        col = 1
+        '''
         return redirect('success')
     return render(request, 'student/base.html', {"dept":dept, "div":div})
 
